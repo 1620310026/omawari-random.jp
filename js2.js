37fb18ea51d9c6c7a8c396fb8ebc9066834f3cd2
1620310026 <138671662+1620310026@users.noreply.github.com>
2023-08-30 22:11:35 +0900

    Update js2.js

--- js2.js
+++ js2.js
@@ -20,72 +20,25 @@ fetch('setsuzoku.json')
         console.error('Error loading station connections:', error);
     });
 
-
-
-
-// Generate a random goal station
-function generateRandomGoal(availableStations) {
-    if (availableStations) {
-        return availableStations[Math.floor(Math.random() * availableStations.length)];
-    } else {
-        console.error('No available stations provided.');
-        return '';
-    }
-}
-
 // Generate a random route and display the result
 function generateRandomRoute() {
     const dropdown = document.getElementById('dropdown');
-    const selectedStation = dropdown.value;
+    const selectedSection = dropdown.value;
 
-    if (stationConnections[selectedStation]) {
-        const goalStation = generateRandomGoal(stationConnections[selectedStation]);
-        const result = generateRandomRoutePython(selectedStation, goalStation);
+    const [startStation, goalStation] = selectedSection.split(" - ");
+    
+    if (stationConnections[startStation] && stationConnections[goalStation]) {
+        let result = [];
+        do {
+            result = generateRandomRoutePython(startStation, goalStation);
+        } while (result.length <= 1);
 
         const output = document.getElementById('output');
         output.textContent = `Random Route: ${result.join(' → ')}`;
     } else {
-        console.error('Selected station not found in stationConnections.');
+        console.error('Selected section not found in stationConnections.');
     }
 }
 
 // Rest of the code remains unchanged
-// Call the Python equivalent to generate a random route
-function generateRandomRoutePython(startStation, goalStation) {
-    const stationList = Object.keys(stationConnections);
 
-    const route = [startStation];
-    const visitedStations = new Set();
-    visitedStations.add(startStation);
-
-    while (true) {
-        const currentStation = route[route.length - 1];
-        const availableStations = stationConnections[currentStation].filter(station => !route.slice(1).includes(station));
-
-        const unvisitedStations = availableStations.filter(station => !visitedStations.has(station));
-
-        if (unvisitedStations.length === 0) {
-            if (route.length === 1) {
-                break;
-            }
-            route.pop();
-            continue;
-        } else {
-            const nextStation = unvisitedStations[Math.floor(Math.random() * unvisitedStations.length)];
-            route.push(nextStation);
-
-            if (nextStation === goalStation) {
-                if (new Set(route).size === 2) {
-                    route.pop();
-                    continue;
-                }
-
-                return route;
-            }
-
-            visitedStations.add(nextStation);
-        }
-    }
-
-    return route;
-}
